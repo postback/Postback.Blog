54a84342fcf20fa4f63f90b105abaf2cdef531ce
Vincent Mouton <mouton.vincent@gmail.com>
2012-02-03 22:55:02 +0100

    Disqus shortname in app setting

--- Postback.Blog/App/Data/MongoSession.cs
+++ Postback.Blog/App/Data/MongoSession.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using Norm;
-using Norm.Collections;
 using Norm.Linq;
 using Norm.Responses;
 
---
+++ Postback.Blog/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+
+public static class HtmlHelperExtensions
+{
+    public static string AppSetting(this HtmlHelper helper, string key)
+    {
+        return ConfigurationManager.AppSettings.AllKeys.Any(k => k == key)
+                   ? ConfigurationManager.AppSettings[key]
+                   : string.Empty;
+    }
+}
