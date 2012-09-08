5b211a5d5385f1449eada1a69018e4c80f834926
Vincent Mouton <mouton.vincent@gmail.com>
2012-09-08 08:04:28 +0200

    Changed BaseTest to use Rhino mocks

--- Postback.Blog.Tests/BaseTest.cs
+++ Postback.Blog.Tests/BaseTest.cs
@@ -1,9 +1,9 @@
 ﻿using System.Web.Mvc;
 using AutoMapper;
-using Moq;
 using NUnit.Framework;
 using Postback.Blog.App.DependencyResolution;
 using Postback.Blog.App.Mapping;
+using Rhino.Mocks;
 using StructureMap;
 
 namespace Postback.Blog.Tests
@@ -19,14 +19,43 @@ namespace Postback.Blog.Tests
             SetStructureMapDependencyResolver();
         }
 
-        public static T M<T>() where T : class
+        [TestFixtureSetUp]
+        public void TestFixtureSetup()
         {
-            return Mock.Of<T>();
         }
 
-        public void InjectMock<T>() where T : class
+        /// <summary>
+        /// Create a mock
+        /// </summary>
+        /// <typeparam name="T">Type to be mocked</typeparam>
+        /// <param name="argumentsForConstructor">Constructor arguments</param>
+        /// <returns>TMessage</returns>
+        protected static T M<T>(params object[] argumentsForConstructor) where T : class
         {
-            ObjectFactory.Inject(typeof(T), M<T>());
+            return MockRepository.GenerateMock<T>(argumentsForConstructor);
+        }
+
+        /// <summary>
+        /// Create a dynamic mock
+        /// </summary>
+        /// <typeparam name="T">Type to be mocked</typeparam>
+        /// <param name="argumentsForConstructor">Constructor arguments</param>
+        /// <returns>TMessage</returns>
+        protected static T DM<T>(params object[] argumentsForConstructor) where T : class
+        {
+            var mocks = new MockRepository();
+            return mocks.DynamicMock<T>(argumentsForConstructor);
+        }
+
+        /// <summary>
+        /// Create a stub
+        /// </summary>
+        /// <typeparam name="T">Type to be stubbed</typeparam>
+        /// <param name="argumentsForConstructor">Constructor arguments</param>
+        /// <returns>TMessage</returns>
+        protected static T S<T>(params object[] argumentsForConstructor) where T : class
+        {
+            return MockRepository.GenerateStub<T>(argumentsForConstructor);
         }
 
         public void SetStructureMapDependencyResolver()
