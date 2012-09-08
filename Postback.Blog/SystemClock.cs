8720288f4d96efbad83a008343a4a4a067c8fcb2
Vincent Mouton <mouton.vincent@gmail.com>
2012-09-08 08:01:15 +0200

    SystemClock interface and default impl

---
+++ Postback.Blog/SystemClock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Postback.Blog
+{
+    public class SystemClock : ISystemClock
+    {
+        public DateTime Now()
+        {
+            return DateTime.Now;
+        }
+    }
+}
