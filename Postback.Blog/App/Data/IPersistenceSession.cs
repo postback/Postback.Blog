f1f0168468cc99433f0ef7bd73a74a14c211da78
Vincent Mouton <mouton.vincent@gmail.com>
2012-09-08 12:08:28 +0200

    Changed IPersistenceSession signature

--- Postback.Blog/App/Data/IPersistenceSession.cs
+++ Postback.Blog/App/Data/IPersistenceSession.cs
@@ -10,12 +10,11 @@ namespace Postback.Blog.App.Data
         void Delete<T>(T item) where T : class, new();
         void DeleteAll<T>() where T : class, new();
         T Single<T>(System.Linq.Expressions.Expression<Func<T, bool>> expression) where T : class, new();
+        IQueryable<T> Find<T>(System.Linq.Expressions.Expression<Func<T, bool>> expression) where T : class, new();
         IQueryable<T> All<T>() where T : class, new();
         T Add<T>(T item) where T : class, new();
         void Save<T>(T item) where T : class, new();
         void Add<T>(IEnumerable<T> items) where T : class, new();
         void Update<T>(T item) where T : class, new();
-        void InsertIntoCollection(object item, string name);
-        T MapReduce<T>(string map, string reduce);
     }
 }
