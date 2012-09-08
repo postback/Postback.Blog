a865a73c7a80cb9ec03053debec3b73128da9a00
Vincent Mouton <mouton.vincent@gmail.com>
2012-09-08 07:49:19 +0200

    BaseHttpModule for unit testing

---
+++ Postback.Blog/App/Bootstrap/BaseHttpModule.cs
@@ -0,0 +1,38 @@
+﻿using System.Web;
+
+namespace Postback.Blog.App.Bootstrap
+{
+    /// <summary>
+    /// Inheriting from this Base IHttpModule, allows us to unit test the implementing module
+    /// </summary>
+    public abstract class BaseHttpModule : IHttpModule
+    {
+        public void Init(HttpApplication app)
+        {
+            app.BeginRequest += (sender, e) => OnBeginRequest(new HttpContextWrapper(((HttpApplication)sender).Context));
+            app.Error += (sender, e) => OnError(new HttpContextWrapper(((HttpApplication)sender).Context));
+            app.EndRequest += (sender, e) => OnEndRequest(new HttpContextWrapper(((HttpApplication)sender).Context));
+            app.PostAuthenticateRequest += (sender, e) => OnPostAuthenticateRequest(new HttpContextWrapper(((HttpApplication)sender).Context));
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public virtual void OnBeginRequest(HttpContextBase context)
+        {
+        }
+
+        public virtual void OnError(HttpContextBase context)
+        {
+        }
+
+        public virtual void OnEndRequest(HttpContextBase context)
+        {
+        }
+
+        public virtual void OnPostAuthenticateRequest(HttpContextBase context)
+        {
+        }
+    }
+}
