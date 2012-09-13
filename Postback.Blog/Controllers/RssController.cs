b7f9acb2be136f640aa84b69e012a7acb37cc00a
Vincent Mouton <mouton.vincent@gmail.com>
2012-09-13 21:08:52 +0200

    Fix RSS feed

--- Postback.Blog/Controllers/RssController.cs
+++ Postback.Blog/Controllers/RssController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.ServiceModel.Syndication;
 using System.Web.Mvc;
+using Microsoft.Practices.ServiceLocation;
 using Postback.Blog.App.Data;
 using Postback.Blog.App.Mvc;
 using Postback.Blog.Models;
@@ -20,11 +21,12 @@ namespace Postback.Blog.Controllers
 
         public ActionResult Index()
         {
+            var clock = ServiceLocator.Current.GetInstance<ISystemClock>();
             var feed = new SyndicationFeed() { Title = new TextSyndicationContent("Interrupted: postback.be RSS Feed"), Description = new TextSyndicationContent("Interrupted: postback.be RSS Feed") };
-            feed.Items = session.All<Post>().Where(
-                p => p.Active && (p.PublishFrom == null || p.PublishFrom <= DateTime.Now)).OrderByDescending(
-                    p => p.Created).Take(PAGE_SIZE)
-                .Select(p => new SyndicationItem(p.Title, p.Body, new Uri("http://" + Request.Url.Host + "/read/" + p.Uri)){PublishDate = p.Created});
+            var items =
+                session.All<Post>().Where(p => p.Active && (p.PublishFrom == null || p.PublishFrom <= clock.Now())).
+                    OrderByDescending(p => p.Created).Take(PAGE_SIZE).ToList();
+            feed.Items = items.Select(p => new SyndicationItem(p.Title, p.Body, new Uri("http://" + Request.Url.Host + "/read/" + p.Uri)) { PublishDate = p.Created });
             return new RssActionResult() { Feed = feed };
         }
 
