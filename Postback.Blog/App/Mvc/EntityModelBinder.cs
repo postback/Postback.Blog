4ed9e094e31d2f972220851aa0804b452d01eb96
Vincent Mouton <mouton.vincent@gmail.com>
2012-09-15 05:49:34 +0200

    Entity Model binder

---
+++ Postback.Blog/App/Mvc/EntityModelBinder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Postback.Blog.App.Data;
+using Postback.Blog.Models;
+
+namespace Postback.Blog.App.Mvc
+{
+    public class EntityModelBinder<TEntity> : DefaultModelBinder where TEntity : Entity,new ()
+    {
+        readonly IPersistenceSession persistenceSession;
+
+        public EntityModelBinder(IPersistenceSession persistenceSession)
+        {
+            this.persistenceSession = persistenceSession;
+        }
+
+        protected override object CreateModel(ControllerContext controllerContext, ModelBindingContext bindingContext, Type modelType)
+        {
+            if (modelType.CanBeCastTo<Entity>())
+            {
+                var id = GetModelId(bindingContext);
+                if (!string.IsNullOrEmpty(id))
+                {
+                    return persistenceSession.Get<TEntity>(id);
+                }
+            }
+
+            return base.CreateModel(controllerContext, bindingContext, modelType);
+        }
+
+        protected override object GetPropertyValue(ControllerContext controllerContext, ModelBindingContext bindingContext, System.ComponentModel.PropertyDescriptor propertyDescriptor, IModelBinder propertyBinder)
+        {
+            var entity = bindingContext.ModelMetadata.Model as Entity;
+            if (entity != null)
+            {
+                var id = GetModelId(bindingContext);
+                if (!string.IsNullOrEmpty(id) && id != entity.Id)
+                {
+                    bindingContext.ModelMetadata.Model = null;
+                }
+            }
+            return base.GetPropertyValue(controllerContext, bindingContext, propertyDescriptor, propertyBinder);
+        }
+
+
+        private static string GetModelId(ModelBindingContext bindingContext)
+        {
+            var fullPropertyKey = CreateSubPropertyName(bindingContext.ModelName, "Id");
+            if (!bindingContext.ValueProvider.ContainsPrefix(fullPropertyKey))
+            {
+                return null;
+            }
+
+            var result = bindingContext.ValueProvider.GetValue(fullPropertyKey);
+            if (result == null)
+            {
+                return null;
+            }
+
+            var idAsObject = result.ConvertTo(typeof(String));
+            if (idAsObject == null)
+            {
+                return null;
+            }
+
+            return idAsObject as String;
+        }
+
+        protected virtual void ValidateEntity(ModelBindingContext bindingContext, ControllerContext controllerContext, object entity)
+        {
+        }
+
+        // For unit tests
+        public void SetModelBinderDictionary(ModelBinderDictionary modelBinderDictionary)
+        {
+            Binders = modelBinderDictionary;
+        }
+    }
+}
