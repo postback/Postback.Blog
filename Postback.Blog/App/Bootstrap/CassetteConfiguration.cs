036b5aebbf81e300f5f26b6e1ae36d33d0c8b522
Vincent Mouton <mouton.vincent@gmail.com>
2012-11-08 16:56:08 +0100

    Upgrade to MVC4 and remove cassette

--- Postback.Blog/App/Bootstrap/CassetteConfiguration.cs
+++ Postback.Blog/App/Bootstrap/CassetteConfiguration.cs
@@ -1,18 +1,18 @@
-﻿using Cassette;
-using Cassette.Scripts;
-using Cassette.Stylesheets;
+﻿//using Cassette;
+//using Cassette.Scripts;
+//using Cassette.Stylesheets;
 
-namespace Postback.Blog
-{
-    /// <summary>
-    /// Configures the Cassette asset modules for the web application.
-    /// </summary>
-    public class CassetteConfiguration : IConfiguration<BundleCollection>
-    {
-        public void Configure(BundleCollection configurable)
-        {
-            configurable.Add<StylesheetBundle>("css");
-            configurable.Add<ScriptBundle>("js");
-        }
-    }
-}
+//namespace Postback.Blog
+//{
+//    /// <summary>
+//    /// Configures the Cassette asset modules for the web application.
+//    /// </summary>
+//    public class CassetteConfiguration : IConfiguration<BundleCollection>
+//    {
+//        public void Configure(BundleCollection configurable)
+//        {
+//            configurable.Add<StylesheetBundle>("css");
+//            configurable.Add<ScriptBundle>("js");
+//        }
+//    }
+//}
