d292018e3ed46e580e12cc185238a5e4815079f0
Vincent Mouton <mouton.vincent@gmail.com>
2012-11-13 22:06:59 +0100

    SquishIt configured to run in memory

---
+++ Postback.Blog/App/Bootstrap/SquishItBootstrap.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Postback.Blog.App.Mapping;
+using SquishIt.Framework;
+
+namespace Postback.Blog.App.Bootstrap
+{
+    public class SquishItBootstrap : IStartUpTask
+    {
+        public void Configure()
+        {
+            //Front-end
+            Bundle.JavaScript()
+            .Add("~/js/lib/modernizr-2.6.2.js")
+            .Add("~/js/lib/jquery.validate.js")
+            .Add("~/js/lib/jquery.validate.unobtrusive.js")
+            .Add("~/js/app/core.js")
+            .AsCached("frontendscripts", "~/assets/js/frontendscripts");
+
+            Bundle.Css()
+           .Add("~/css/styles.css")
+           .AsCached("frontend", "~/assets/css/frontend");
+
+            //back-end
+            Bundle.JavaScript()
+            .Add("~/js/lib/modernizr-2.6.2.js")
+            .Add("~/js/lib/jquery.validate.js")
+            .Add("~/js/lib/jquery.validate.unobtrusive.js")
+            .Add("~/js/app/admin.js")
+            .AsCached("backendscripts", "~/assets/js/backendscripts");
+
+            //Bundle.Css()
+           //.Add("~/css/admin/styles.css")
+            //.AsCached("backend", "~/assets/css/backend");
+        }
+    }
+}
---
+++ Postback.Blog/Controllers/AssetController.cs
@@ -0,0 +1,26 @@
+﻿using SquishIt.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Postback.Blog.Controllers
+{
+    public class AssetsController : Controller
+    {
+        public ActionResult Js(string id)
+        {
+            // Set max-age to a year from now
+            Response.Cache.SetMaxAge(TimeSpan.FromDays(365));
+            return Content(Bundle.JavaScript().RenderCached(id), "text/javascript");
+        }
+
+        public ActionResult Css(string id)
+        {
+            // Set max-age to a year from now
+            Response.Cache.SetMaxAge(TimeSpan.FromDays(365));
+            return Content(Bundle.Css().RenderCached(id), "text/css");
+        }
+    }
+}
